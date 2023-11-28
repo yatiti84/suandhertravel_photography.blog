4f03f2fca5d9e27587eceb468f932c5247ac5d46
susu <yatiti84@gmail.com>
2023-11-28 14:15:45 +0800

    fix: query post by tag

--- dvg/frontend/src/components/PostsByTag.vue
+++ dvg/frontend/src/components/PostsByTag.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <h2>Posts in #{{ $route.params.tag }}</h2>
+    <p v-if="error">Something went wrong...</p>
+    <p v-if="loading">Loading...</p>
     <PostList :posts="posts" v-if="posts" />
   </div>
 </template>
@@ -8,46 +10,58 @@
 <script>
 import PostList from '@/components/PostList'
 import gql from 'graphql-tag'
+import { useQuery } from '@vue/apollo-composable'
+import { useRoute } from 'vue-router'
+
+const postsByTagQuery = gql`query ($tag: String!) {
+  postsByTag(tag: $tag) {
+    title
+    subtitle
+    publishDate
+    published
+    metaDescription
+    slug
+    author {
+      user {
+        username
+        firstName
+        lastName
+      }
+    }
+    tags {
+    slug
+      name
+    }
+    categories {
+    slug
+      name
+    }
+  }
+},
+`
+
 
 export default {
-  async created () {
-    const posts = await this.$apollo.query({
-      query: gql`query ($tag: String!) {
-        postsByTag(tag: $tag) {
-          title
-          subtitle
-          publishDate
-          published
-          metaDescription
-          slug
-          author {
-            user {
-              username
-              firstName
-              lastName
-            }
-          }
-          tags {
-            name
-          }
-          categories {
-          name
-          }
-        }
-      }`,
-      variables: {
-        tag: this.$route.params.tag,
-      },
-    })
-    this.posts = posts.data.postsByTag
-  },
   name: 'PostsByTag',
+  setup() {
+    const route = useRoute()
+    const routeName = route.params.tag
+    const { result, loading, error } = useQuery(postsByTagQuery, {
+      tag: routeName,
+    }
+    );
+    return {
+      posts: result.value ? result.value.postsByTag : null,
+      loading,
+      error
+    }
+  },
   components: {
     PostList,
   },
-  data () {
+  data() {
     return {
-      posts: null,
+      postsByTag: null,
     }
   },
 }
